6a1315df2be6dfde1eaeffaf609001fba7f42979
salmaqnsgt <nurussalamahqonaah@gmail.com>
2022-07-01 21:48:20 +0700

    Modified register and login form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_30_140819) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_01_140034) do
   create_table "book_payments", force: :cascade do |t|
     t.string "payment_proof"
     t.boolean "is_valid_payment"
@@ -73,6 +73,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_30_140819) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
