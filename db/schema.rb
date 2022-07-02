4a9c9193dd1a39f85de244d71ed6b2913cbb7160
salmaqnsgt <nurussalamahqonaah@gmail.com>
2022-07-02 12:17:42 +0700

    Tested user validate existing address, role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_01_140034) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_02_043606) do
   create_table "book_payments", force: :cascade do |t|
     t.string "payment_proof"
     t.boolean "is_valid_payment"
@@ -71,8 +71,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_01_140034) do
     t.string "name"
     t.string "type"
     t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,16 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before do
-    @user = User.create(email: "customertest@test.com", password: "sasasasa", password_confirmation: "sasasasa", name:"sals")
+    @user = User.create(email: "customertest@test.com", password: "sasasasa", password_confirmation: "sasasasa", name:"sals", role:"publisher", address:"solo")
   end
   describe "creation" do
-
     it "can be created" do
       expect(@user).to be_valid
     end
 
-    it "cannot be created witout first_name, last_name" do
+    it "cannot be created witout name, role, address" do
       @user.name = nil
+      @user.role = nil
+      @user.address = nil
+      p @user
       expect(@user).to_not be_valid
     end
   end
