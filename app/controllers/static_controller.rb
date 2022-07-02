a10a381accf12e61d5028ec1c5da9c31e02f6426
salmaqnsgt <nurussalamahqonaah@gmail.com>
2022-07-02 16:59:40 +0700

    Edited route for user role

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,13 @@
 class StaticController < ApplicationController
     def homepage
+        if current_user
+            if current_user.role == "customer"
+                redirect_to root_path
+            elsif current_user.role == "publisher"
+                redirect_to books_path
+            end
+        else
+            redirect_to new_user_session_path
+        end
     end
 end
